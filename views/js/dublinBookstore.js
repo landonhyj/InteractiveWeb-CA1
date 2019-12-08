578fc24baff529dd3dbee7255921b2c115da1d19
landonhyj <landonhyj@gmail.com>
2019-12-08 10:39:44 +0000

    10th commit

--- views/js/dublinBookstore.js
+++ views/js/dublinBookstore.js
@@ -1,9 +1,9 @@
-function getParentTag(oNode, sParentType) {
-	var oParent = oNode.parentNode;
-	while (oParent) {
-		if (oParent.nodeName == sParentType)
-			return oParent;
-		oParent = oParent.parentNode;
-	};
-	return oParent;
-};
+// function getParentTag(oNode, sParentType) {
+// 	var oParent = oNode.parentNode;
+// 	while (oParent) {
+// 		if (oParent.nodeName == sParentType)
+// 			return oParent;
+// 		oParent = oParent.parentNode;
+// 	};
+// 	return oParent;
+// };
